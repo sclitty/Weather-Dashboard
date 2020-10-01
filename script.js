54865122a17ac81ce84334eea1c58a05b6f17d61
Samuel Clitty <samclitty@gmail.com>
2020-09-30 21:28:14 -0700

    updated files and added html frame to dynamically add data

---
+++ script.js
@@ -0,0 +1,16 @@
+// Verifying that script is linked
+console.log("HELLO WORLD");
+
+// DOM ELEMENTS
+
+
+
+// SETTING VARIABLES
+var cityName
+
+
+
+// API LINK
+var city = $("city-entry").val();
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + city + "q=&appid=" + APIKey;
+var APIKey = "7c97e50f070e269534e301c206d179f4";
